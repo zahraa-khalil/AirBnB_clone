c5f1519c8b106b5bcda428cf72aafaa838101d45
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2024-05-19 01:44:35 +0300

    5

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Defines the BaseModel class."""
+import models
 from uuid import uuid4
 from datetime import datetime
 
@@ -26,10 +27,12 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.today()
             self.updated_at = datetime.today()
+            models.storage.new(self)
 
     def save(self):
         """Updates updated_at with the current datetime"""
         self.updated_at = datetime.today()
+        models.storage.save()
 
     def __str__(self):
         """Return the print/str representation of the BaseModel instance."""
