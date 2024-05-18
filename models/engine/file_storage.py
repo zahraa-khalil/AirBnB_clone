1e8fcbeda97af914658ec7dff03bb0ac351ae64a
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2024-05-19 01:55:00 +0300

    5.2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,13 +3,16 @@
 import json
 from models.base_model import BaseModel
 
+
 class FileStorage:
+
     """Represent an abstracted storage engine.
 
     Attributes:
         __file_path (str): The name of the file to save objects to.
         __objects (dict): A dictionary of instantiated objects.
     """
+
     __file_path = "file.json"
     __objects = {}
 
