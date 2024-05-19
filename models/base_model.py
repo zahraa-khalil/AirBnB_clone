222836231949a7ce58c948344ea4917fba4e04c0
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-05-19 23:23:47 +0300

    update

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """Defines the BaseModel class."""
 import models
@@ -7,18 +6,17 @@ from datetime import datetime
 
 
 class BaseModel:
-    """Represents the BaseModel of the HBnB project."""
+    """Base class for AirBnB project"""
 
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
+        """Initialize a new Base instance with a unique ID and timestamps.
+
         Args:
             *args (any): Unused.
             **kwargs (dict): Key/value pairs of attributes.
         """
         tform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
+
         if len(kwargs) != 0:
             for k, v in kwargs.items():
                 if k == "created_at" or k == "updated_at":
@@ -26,25 +24,25 @@ class BaseModel:
                 else:
                     self.__dict__[k] = v
         else:
+            self.id = str(uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
             models.storage.new(self)
 
     def save(self):
-        """Update updated_at with the current datetime."""
+        """Updates updated_at with the current datetime"""
         self.updated_at = datetime.today()
         models.storage.save()
-
-    def to_dict(self):
-        """Return the dictionary of the BaseModel instance.
-        Includes the key/value pair __class__ representing
-        the class name of the object.
-        """
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
 
     def __str__(self):
         """Return the print/str representation of the BaseModel instance."""
         clname = self.__class__.__name__
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__"""
+        new_dict = self.__dict__.copy()
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        new_dict["__class__"] = self.__class__.__name__
+        return new_dict
