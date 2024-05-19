793da18fa55b4b813822eb3d4fd641a881885c1d
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2024-05-19 19:47:53 +0300

    add

--- console.py
+++ console.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+"""Defines the HBnB console."""
+
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
-    """HBNB command interpreter"""
+    """HBNB command interpreter
+    Attributes:
+        prompt (str): The command prompt.
+    """
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
@@ -17,6 +23,7 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing on empty input line"""
         pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
