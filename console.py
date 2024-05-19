37c44f17c0da8f8f78196db8c8864072fbd4eccc
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-05-19 23:25:30 +0300

    update

--- console.py
+++ console.py
@@ -14,6 +14,7 @@ from models.review import Review
 
 
 def parse(arg):
+    """Parse the command line arguments"""
     curly_braces = re.search(r"\{(.*?)\}", arg)
     brackets = re.search(r"\[(.*?)\]", arg)
     if curly_braces is None:
