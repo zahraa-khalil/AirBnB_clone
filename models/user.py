d1ea104c95b533501f8b3f8fd1245a622fe5404a
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-05-19 22:22:17 +0300

    update-user

--- models/user.py
+++ models/user.py
@@ -4,19 +4,15 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """User class for AirBnB project"""
+    """Initialize a new User instance.
 
-    def __init__(self, email, password, first_name, last_name):
-        """Initialize a new User instance.
-
-        Args:
-            email (str): User's email.
-            password (str):  User's password.
-            first_name (str): User's first name.
-            last_name (str): User's last name.
-        """
-        super().__init__()
-        self.email = email
-        self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
+    Args:
+        email (str): User's email.
+        password (str):  User's password.
+        first_name (str): User's first name.
+        last_name (str): User's last name.
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
